cba9d1ee62cad9980eef08b5fca0c3c91be4f61b
hong04016216-star <hong04016216@gmail.com>
2026-08-01 18:29:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 const tb = document.getElementById("tb");
 const q = document.getElementById("q");
 
-const CSV_URL =
-"https://docs.google.com/spreadsheets/d/e/2PACX-1vQ6GyYQJeFh_Zm-vDQH7u4f4b3dKeDgoOVok0LOn6Tc6XqMT6dbw5DJpZ00NzZeydEgWY4VjMG49h-c/pub?gid=782401085&single=true&output=csv";
+const API_URL =
+"https://script.google.com/macros/s/AKfycbw4xrHBLEuF0vBRb02H5YQs_KOpfctLUw7VYurL5qDSmp-XPCjjxt-cMCAEXbaVKMo/exec";
 
 let vocab = [];
 
 // 播放 MP3
-function speak(file) {
+function speak(file){
 
     const audio = new Audio("audio/" + file);
 
-    audio.play().catch(() => {
+    audio.play().catch(()=>{
         alert("找不到 MP3：" + file);
     });
 
 }
 
 // 顯示資料
-function render(list) {
+function render(list){
 
     tb.innerHTML = "";
 
-    list.forEach(r => {
+    list.forEach(r=>{
 
         const tr = document.createElement("tr");
 
         tr.innerHTML = `
-        <td>${r["中文"]}</td>
-        <td>${r["泰文"]}</td>
-        <td>${r["拼音"]}</td>
-        <td><button>🇹🇼🔊</button></td>
-        <td><button>🇹🇭🔊</button></td>
+            <td>${r["中文"]}</td>
+            <td>${r["泰文"]}</td>
+            <td>${r["拼音"]}</td>
+            <td><button>🇹🇼🔊</button></td>
+            <td><button>🇹🇭🔊</button></td>
         `;
 
         const b = tr.querySelectorAll("button");
 
-        b[0].onclick = () => speak(r["中文"] + ".mp3");
+        b[0].onclick = ()=>speak(r["中文"] + ".mp3");
 
-        b[1].onclick = () => {
+        b[1].onclick = ()=>{
             alert("泰文 MP3 尚未建立");
         };
 
@@ -48,43 +48,34 @@ function render(list) {
 
 }
 
-// 讀 Google 試算表
-Papa.parse(CSV_URL, {
+// 讀取 Google 試算表
+fetch(API_URL)
+.then(r=>r.json())
+.then(data=>{
 
-    download: true,
-    header: true,
-    
-    complete: function(results) {
+    console.log(data);
 
-    console.log(results);
-
-    vocab = results.data.filter(r => r["中文"]);
-
-    console.log(vocab);
+    vocab = data;
 
     render(vocab);
 
-},
+})
+.catch(err=>{
 
+    console.error(err);
 
-    error: function(err) {
-
-        console.error(err);
-
-        alert("讀取 Google 試算表失敗");
-
-    }
+    alert("無法讀取 Google 試算表");
 
 });
 
 // 搜尋
-q.addEventListener("input", function() {
+q.addEventListener("input",()=>{
 
     const t = q.value.toLowerCase();
 
     render(
 
-        vocab.filter(r =>
+        vocab.filter(r=>
 
             Object.values(r)
                 .join(" ")
